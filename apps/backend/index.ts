6f0057252dc1d7bf52037aaec5f9075b1b42995c
Pratik Kale <pratikkale7661@gmail.com>
2025-09-17 16:25:58 +0530

    prisma added, schema done, hooks done

--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -1,1 +1,14 @@
-console.log("Hello via Bun!");
+import express from "express";
+import { prisma } from "db/prisma";
+import cors from "cors"
+
+
+const app = express()
+app.use(express.json());
+app.use(cors());
+
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}!`)
+})
---
+++ apps/hooks/index.ts
@@ -0,0 +1,36 @@
+import express from "express"
+import { prisma } from "db/prisma";
+
+const app = express();
+app.use(express.json());
+
+// password logic 
+app.post("/hooks/catch/:userId/:zapId", async (req, res) => {
+    const userId = req.params.userId;
+    const zapId = req.params.zapId;
+    const body = req.body;
+
+    // store in db a new trigger
+    await prisma.$transaction(async tx => {
+        const run = await tx.zapRun.create({
+            data: {
+                zapId: zapId,
+                metadata: body
+            }
+        });;
+
+        await tx.zapRunOutbox.create({
+            data: {
+                zapRunId: run.id
+            }
+        })
+    })
+    res.json({
+        message: "Webhook received"
+    })
+})
+
+const PORT = 3002;
+app.listen(PORT, () => {
+    console.log(`hooks listening on port ${PORT}!`)
+});
