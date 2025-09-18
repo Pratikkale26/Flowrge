1aab23c740ec823c533004a5ba167f57e0e8c563
Pratik Kale <pratikkale7661@gmail.com>
2025-09-18 13:47:14 +0530

    added processor - to put zaps into kafka

---
+++ apps/processor/index.ts
@@ -0,0 +1,40 @@
+import { prisma } from "db/prisma";
+import { Kafka } from "kafkajs";
+
+const TOPIC_NAME="zap-events"
+const kafka = new Kafka({
+  clientId: 'outbox-processor',
+  brokers: ['localhost:9092']
+})
+
+async function main() {
+    const producer = kafka.producer();
+    await producer.connect();
+
+    while (1) {
+        // get the zap from outbox
+        const pendingRows = await prisma.zapRunOutbox.findMany({
+            where: {},
+            take: 10
+        })
+
+        // send to kafka
+        producer.send({
+            topic: TOPIC_NAME,
+            messages: pendingRows.map(r => ({
+                value: r.zapRunId
+            }))
+        })
+
+        // delete the zap entry from outbox
+        await prisma.zapRunOutbox.deleteMany({
+            where: {
+                id: {
+                    in: pendingRows.map(r => r.id)
+                }
+            }
+        })
+    }   
+}
+
+main();
