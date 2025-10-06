30b91a5fca6c3e9df1748abe2875725941da16f7
Pratik Kale <pratikkale7661@gmail.com>
2025-10-06 10:59:24 +0530

    wallet adaptors added

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-// import { Appbar } from "../components/Appbar";
+import WalletContextProvider from "../components/WalletAdaptors";
+import { Appbar } from "../components/Appbar";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -35,8 +36,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {/* <Appbar/> */}
-        {children}
+        <WalletContextProvider>
+          <Appbar/>
+          {children}
+        </WalletContextProvider>
       </body>
     </html>
   );
--- apps/web/components/Appbar.tsx
+++ apps/web/components/Appbar.tsx
@@ -3,10 +3,13 @@
 import { useRouter } from "next/navigation";
 import { LinkButton } from "./buttons/LinkButton"
 import { PrimaryButton } from "./buttons/PrimaryButton";
+import { WalletDisconnectButton, WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import { useWallet } from "@solana/wallet-adapter-react";
+
 
 export const Appbar = () => {
     const router = useRouter();
-
+    const { publicKey } = useWallet();
     return <div className="flex border-b justify-between">
         <div className="text-2xl p-2 font-semibold">
             Flowrge
@@ -15,6 +18,14 @@ export const Appbar = () => {
             <LinkButton onClick={() => {}}>Contact Sales</LinkButton>
             <LinkButton onClick={() => {router.push("/login")}}>Login</LinkButton>
             <PrimaryButton onClick={() => {router.push("/signup")}}>Sign Up</PrimaryButton>
+            {/* Wallet Connect */}
+          <div className="hidden sm:flex">
+            {publicKey ? (
+              <WalletDisconnectButton className="!bg-destructive hover:!bg-destructive/90" />
+            ) : (
+              <WalletMultiButton className="!bg-primary hover:!bg-primary/90" />
+            )}
+          </div>
         </div>
     </div>
 }
